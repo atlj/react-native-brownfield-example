e44f8d94a36a64d9bab9fb87f2fa347d5717367c
Burak Güner <burak.guner@callstack.com>
2024-01-29 20:59:07 +0300

    android setup

--- android/app/src/main/java/dev/burakguner/composetest/ReactNativeActivity.kt
+++ android/app/src/main/java/dev/burakguner/composetest/ReactNativeActivity.kt
@@ -1,34 +1,66 @@
 package dev.burakguner.composetest
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import dev.burakguner.composetest.ui.theme.ComposeTestTheme
+import com.facebook.react.JSEngineResolutionAlgorithm
+import com.facebook.react.PackageList
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactPackage
+import com.facebook.react.ReactRootView
+import com.facebook.react.common.LifecycleState
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
+import com.facebook.soloader.SoLoader
 
-class ReactNativeActivity : ComponentActivity() {
+class ReactNativeActivity : Activity(), DefaultHardwareBackBtnHandler {
+    private var reactRootView: ReactRootView? = null
+    private var reactInstanceManager: ReactInstanceManager? = null
+
     companion object {
         fun createIntent(activity: ComponentActivity) =
             android.content.Intent(activity, ReactNativeActivity::class.java)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ComposeTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Text("this will be React Native in a second")
-                }
-            }
-        }
+
+        reactRootView = ReactRootView(this);
+
+        SoLoader.init(this, false)
+        val packages: List<ReactPackage> = PackageList(application).packages
+
+        reactInstanceManager = ReactInstanceManager.builder()
+            .setApplication(application)
+            .setCurrentActivity(this)
+            .setBundleAssetName("index.android.bundle")
+            .setJSMainModulePath("index")
+            .setJSEngineResolutionAlgorithm(JSEngineResolutionAlgorithm.HERMES)
+            .addPackages(packages)
+            .setUseDeveloperSupport(BuildConfig.DEBUG)
+            .setInitialLifecycleState(LifecycleState.RESUMED)
+            .build()
+
+        reactRootView!!.startReactApplication(reactInstanceManager, "Brownfield", null);
+        setContentView(reactRootView);
+    }
+
+    override fun onPause() {
+        super.onPause()
+        reactInstanceManager?.onHostPause(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        reactInstanceManager?.onHostResume(this, this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        reactInstanceManager?.onHostDestroy(this)
+        reactRootView?.unmountReactApplication()
+    }
+
+    override fun invokeDefaultOnBackPressed() {
+        super.onBackPressed()
     }
 }
